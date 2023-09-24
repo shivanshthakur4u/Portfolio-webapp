78aa2cb5ec2cbbffc8410cf8147556c41217b9db
shivanshthakur4u <boyslike2play@gmail.com>
2023-09-24 12:37:46 +0530

    fixes for experience design

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -5,14 +5,14 @@ import Liicon from './Liicon'
 const Details=({position, company, companyLink, time, address, work})=>{
     const ref = useRef(null)
 return(
-   <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between'>
+   <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] md:w-[80%] sm:mx-auto flex flex-col items-center justify-between'>
    <Liicon reference={ref}/>
      <motion.div initial={{y:50}} whileInView={{y:0}} transition={{duration:0.5, type:"spring"}}>
         <h3 className=' capitalize font-bold text-2xl'>{position}&nbsp;<a href={companyLink} target='_blank' className='text-primary capitalize'>@{company}</a></h3>
-        <span className=' capitalize font-medium text-dark/75'>
+        <span className=' capitalize font-medium text-dark/75 dark:text-light/75 xs:text-sm'>
             {time} | {address}
         </span>
-        <p className='font-medium w-full'>
+        <p className='font-medium w-full md:text-sm'>
             {work}
         </p>
         </motion.div>
@@ -25,10 +25,10 @@ const Experience = () => {
     const {scrollYProgress} = useScroll({target:ref,offset:["start end","center start"]})
   return (
    <div className='my-64'>
-    <h2 className='font-bold text-8xl mb-32 w-full text-center'>Experience</h2>
-    <div ref={ref} className='w-[75%] mx-auto relative'>
-    <motion.div style={{scaleY:scrollYProgress}} className=' absolute left-9 top-0 w-[4px] h-full bg-dark  origin-top'/>
- <ul className='w-full flex flex-col items-center justify-between ml-4'>
+    <h2 className='font-bold text-8xl mb-32 w-full text-center md:text-6xl sm:text-4xl md:mb-16'>Experience</h2>
+    <div ref={ref} className='w-[75%] mx-auto relative lg:w-[90%] md:w-full'>
+    <motion.div style={{scaleY:scrollYProgress}} className=' absolute left-9 top-0 w-[4px] h-full bg-dark dark:bg-light origin-top md:w-[2px] md:left-[30px] xs:left-[20px]'/>
+ <ul className='w-full flex flex-col items-center justify-between ml-4 xs:ml-2'>
     <Details time={"May 2023 - July 2023"}  address={"MIG 401, 11th Cross Road, Panampilly Nagar, Kochi, Kerala, India, 682036"} position={"ReactJS Developer Intern"}
   work={<ul>
   <li>Enhance, maintain and build frontend for applications,</li>
