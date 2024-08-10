39fae029f189151fc95c1871c12f41ca0f253af9
shivanshthakur4u <boyslike2play@gmail.com>
2024-08-10 13:44:19 +0530

    fixed bavbar hamberger

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -44,9 +44,9 @@ const Navbar = () => {
 
     <button className='flex-col items-center justify-center hidden lg:flex' onClick={handleClick}>
     
-      <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm -translate-y-0.5 ${isOpen ? 'rotate-45 translate-y-[4px]':'-translate-y-1'}`}></span>
+      <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm -translate-y-0.5 ${isOpen ? 'rotate-45 translate-y-[9px]':'-translate-y-0.5'}`}></span>
       <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${isOpen ? 'opacity-0':'opacity-100'}`}></span>
-      <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm translate-y-0.5 ${isOpen ? '-rotate-45 -translate-y-[5px]':'translate-y-1'}`}></span>
+      <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm translate-y-0.5 ${isOpen ? '-rotate-45 -translate-y-[5px]':'translate-y-0.5'}`}></span>
     </button>
 
 <div className='w-full flex justify-between items-center lg:hidden'>
